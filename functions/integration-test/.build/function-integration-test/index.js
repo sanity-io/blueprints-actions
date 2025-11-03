a283004c7b3089b78574124db3e7dfddb988915c
Stephen Fisher <stephen.fisher@sanity.io>
2025-11-03 09:47:23 -0700

    remove .build files

--- functions/integration-test/.build/function-integration-test/index.js
+++
@@ -1,9 +0,0 @@
-import { documentEventHandler } from "@sanity/functions";
-const handler = documentEventHandler(async ({ context, event }) => {
-  const time = (/* @__PURE__ */ new Date()).toLocaleTimeString();
-  console.log(`👋 Your Sanity Function was called at ${time}`);
-});
-export {
-  handler
-};
-//# sourceMappingURL=index.js.map

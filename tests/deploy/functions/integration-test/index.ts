1399a165a0e21ab1a0474b31fc4c8fc2f3ca3c22
Stephen Fisher <stephen.fisher@sanity.io>
2026-02-03 13:01:59 -0700

    fix: use users deps instead of creating our own

--- tests/deploy/functions/integration-test/index.ts
+++ tests/deploy/functions/integration-test/index.ts
@@ -1,6 +1,20 @@
-import { documentEventHandler } from '@sanity/functions'
+import {documentEventHandler} from '@sanity/functions'
+import {createClient} from '@sanity/client'
+import chalk from 'chalk'
 
-export const handler = documentEventHandler(async ({ context, event }) => {
+const PAGE_URL_QUERY = `*[_type == "page" && _id == $id] {
+  _type,
+  _id,
+  title,
+  slug,
+  site,
+}`;
+
+export const handler = documentEventHandler(async ({context, event}) => {
     const time = new Date().toLocaleTimeString()
+    const client = createClient({apiVersion: '2024-01-01', ...context.clientOptions})
+    const data = event.data
+    const documents = client.fetch(PAGE_URL_QUERY, {id: data._id})
+    console.log(documents)
     console.log(`👋 Your Sanity Function was called at ${time}`)
 })

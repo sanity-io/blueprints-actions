1ede2d46dfaf89396a32c6a38ad92519fa54eb4e
Stephen Fisher <stephen.fisher@sanity.io>
2026-05-05 09:58:50 -0600

    comments

--- tests/deploy-org/functions/scheduled-test/index.ts
+++ tests/deploy-org/functions/scheduled-test/index.ts
@@ -1,17 +1,6 @@
 import {scheduledEventHandler} from '@sanity/functions'
-import {createClient} from '@sanity/client'
-
-const projectId = process.env.PROJECT_ID
-if (!projectId) {
-    throw new Error('Missing PROJECT_ID environment variable')
-}
 
 export const handler = scheduledEventHandler(({context}) => {
-    const client = createClient({
-        apiVersion: '2025-05-01',
-        projectId,
-        ...context.clientOptions,
-    })
 
     console.log('Scheduled task executed', {local: context.local})
 })
